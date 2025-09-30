d473898b9eb85fd8fa7363487d8db2147d532578
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-09-30 14:55:09 -0500

    update build.lua

--- build.lua
+++ build.lua
@@ -1,7 +1,7 @@
 return {
       name = 'CherryPop',
       config = 'nonsteam',
-      version = '0.3',
+      version = '0.3.1',
       love = '12.0',
       icon = 'assets/icon.png',
       platforms = {'windows','linux','mac'},
