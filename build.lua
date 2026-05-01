2b7aaae7981fc84a5f274e51834aaa5dfd8ae971
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-05-01 17:12:13 -0500

    build number

--- build.lua
+++ build.lua
@@ -1,7 +1,7 @@
 return {
       name = 'CherryPop',
       config = 'nonsteam',
-      version = '0.3.3',
+      version = '0.3.4',
       love = '11.5',
       icon = 'assets/icon.png',
       platforms = {'windows','linux','macos'},
