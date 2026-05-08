1a594ceb7c140ee7d333018fef08d73dc4195dcb
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-05-08 14:11:06 -0500

    add icons to carts!

--- editors/icon.lua
+++ editors/icon.lua
@@ -14,6 +14,18 @@ function icon:draw()
     shove.beginLayer("screen")
         colr(2)
         lg.rectangle("fill",0,0,128,96)
+
+        local sw,sh=64,96/2
+        local w,h=36,48
+        local x,y=sw-w/2,sh-h/2-7
+        hexImg(cartImg,x,y,w,h,1)
+
+        for i=0,15 do
+           colr(i) 
+           local s=6
+           lg.rectangle("fill",i*s,80,s,s)
+        end
+
         bar.draw()
         mouse.draw()
     shove.endLayer()
