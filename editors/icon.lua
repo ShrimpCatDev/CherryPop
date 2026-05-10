58ccc721ff8ade224eea84b54df54a010813f113
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-05-09 22:20:15 -0500

    add color selecting

--- editors/icon.lua
+++ editors/icon.lua
@@ -1,12 +1,42 @@
 local icon={}
 
+local function col(ax,ay,bx,by,aw,ah,bw,bh)
+    return ax<bx+bw and bx<ax and ay<by+bh and by<ay
+end
+
+local function click(mouse,obj)
+    return col(mouse.x,mouse.y,obj.x,obj.y,1,1,obj.w,obj.h)
+end
+
 function icon:enter()
     mouse=require("editors.mouse")
     bar.init()
+    colorSelect={
+        len=16,
+        sel=0,
+        scale=6,
+        x=64-(16*6*0.5),
+        y=80,
+        
+    }
+    colorSelect.w=colorSelect.len*colorSelect.scale
+    colorSelect.h=colorSelect.scale
+    colorSelect.update=function(mouse)
+        local s=colorSelect
+        if click(mouse,s) then
+            local mx,my=mouse.x-s.x,mouse.y-s.y
+            colorSelect.sel=math.floor(mx/s.scale)
+        end
+    end
 end
 
 function icon:update()
     mouse.update()
+    if mouse.x and mouse.y then
+        if love.mouse.isDown(1) then
+            colorSelect.update(mouse)
+        end
+    end
 end
 
 function icon:draw()
@@ -20,11 +50,15 @@ function icon:draw()
         local x,y=sw-w/2,sh-h/2-7
         hexImg(cartImg,x,y,w,h,1)
 
-        for i=0,15 do
+        local len=16
+        for i=0,len-1 do
            colr(i) 
            local s=6
-           lg.rectangle("fill",i*s,80,s,s)
+           lg.rectangle("fill",i*s+colorSelect.x,colorSelect.y,s,s)
         end
+
+        colr(colorSelect.sel)
+        lg.rectangle("fill",16,16,8,8)
 
         bar.draw()
         mouse.draw()
